80c7139d6786dba7f5b0330bdf985952298c4247
Miguel <miguelszcorti@gmail.com>
2014-06-11 14:03:09 -0300

    Added basic validations to the users class, such as: name can't be blank, email needs '@''.', etc.

--- first_app/db/schema.rb
+++ first_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507154901) do
+ActiveRecord::Schema.define(version: 20140611155511) do
 
   create_table "posts", force: true do |t|
     t.string   "content"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(version: 20140507154901) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
