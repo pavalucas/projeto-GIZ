c065b68c0763f8c3c9cf70d778da3afb3fde6f97
Lucas <lucasapava@gmail.com>
2014-06-17 23:21:13 -0300

    Inserção da senha do usuário, criação do formulário de inscrição e página inicial do usuário

--- first_app/config/routes.rb
+++ first_app/config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
 
   resources :users
 
+  root  'static_pages#home'
+
+  match '/help',    to: 'static_pages#help',    via: 'get'
+
+  match '/signup',  to: 'users#new',            via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
