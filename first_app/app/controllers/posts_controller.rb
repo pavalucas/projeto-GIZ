749baa21048e6adf4f0e5e4fd67fddbcdafc2793
Lucas <lucasapava@gmail.com>
2014-07-16 12:35:00 -0300

    Visualizaçao dos posts dos usuarios

---
+++ first_app/app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  before_action :signed_in_user
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "Post created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
+
+    def signed_in_user
+      store_location
+      redirect_to signin_url, notice: "Você não está logado." unless signed_in?
+    end
+end
--- first_app/config/routes.rb
+++ first_app/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
+  resources :posts, only: [:create, :destroy]
+
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
