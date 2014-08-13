6704491e474f96ea6c6a3856fbc4ac9430496487
Miguel <miguelszcorti@gmail.com>
2014-08-13 11:27:10 -0300

    Ajuste do Menu

--- first_app/app/helpers/groups_helper.rb
+++ first_app/app/helpers/groups_helper.rb
@@ -1,10 +1,11 @@
 module GroupsHelper
 	def current_group
-    	remember_token = Group.digest(cookies[:remember_token])
-    	@current_group ||= Group.find_by(remember_token: remember_token)
+    	#remember_token = Group.digest(cookies[:remember_token])
+    	@current_group ||= Group.find_by(name: "Turma 302")
+    	@posts = @current_group.posts.paginate(page: params[:page])
   	end
 
-    def current_user?(group)
+    def current_group?(group)
       group == current_group
     end
 end
