cb0e9b613c238176863380d87444afa3727afd20
Miguel <miguelszcorti@gmail.com>
2014-08-20 19:48:19 -0300

    Calendario

--- first_app/db/schema.rb
+++ first_app/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730155058) do
+ActiveRecord::Schema.define(version: 20140820220509) do
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "groups", force: true do |t|
     t.string   "name"
