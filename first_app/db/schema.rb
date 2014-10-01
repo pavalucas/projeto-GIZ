5b343e1e3fdb99a31e3a04aad2683773fc8d8175
Miguel <miguelszcorti@gmail.com>
2014-10-01 10:43:08 -0300

    Mudanças de css, opção de professor para o usuario

--- first_app/db/schema.rb
+++ first_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820220509) do
+ActiveRecord::Schema.define(version: 20140924140854) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20140820220509) do
     t.string   "remember_token"
     t.boolean  "admin",           default: false
     t.integer  "group_id"
+    t.boolean  "teacher"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
